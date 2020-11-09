b032dc72f310de6d23c152f7d4c2c1b94c781f45
Thomas Kite <thomas.kite@student.manchester.ac.uk>
2020-11-09 19:38:49 +0000

    Minor style update

--- day_1_part_1.py
+++ day_1_part_1.py
@@ -12,5 +12,5 @@ FILENAME = "day_1_data.dat"
 
 if __name__ == "__main__":
     data = np.genfromtxt(FILENAME, dtype='int32')
-    total_sum = sum(data)
+    total_sum = data.sum()
     print("Total sum is {}".format(total_sum))
--- day_1_part_2.py
+++ day_1_part_2.py
@@ -13,21 +13,24 @@ FILENAME = "day_1_data.dat"
 if __name__ == "__main__":
     data = np.genfromtxt(FILENAME, dtype='int32')
     all_freqs = np.zeros(1, dtype='int32')
-    
+
     current_index = 0
     total_iterations = 0
-    
+
     while True:
         if (current_index >= len(data)):
             current_index = 0
         new_freq = all_freqs[-1] + data[current_index]
-        
+
         if new_freq in all_freqs:
             print("Yay!")
             print("First repeated frequency is {}".format(new_freq))
             print("This took {} iterations".format(total_iterations))
             break
-        else:
-            all_freqs = np.append(all_freqs, new_freq)
-            current_index += 1
-            total_iterations += 1
+
+        all_freqs = np.append(all_freqs, new_freq)
+        current_index += 1
+        total_iterations += 1
+
+
+
